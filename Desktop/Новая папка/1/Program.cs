752e82192a620c631d836c203e541bcda0d9b8de
dinur0 <119072459+dinur0@users.noreply.github.com>
2022-12-27 15:33:35 +0300

    Update Program.cs
    programm reworked

--- Desktop/Новая папка/1/Program.cs
+++ Desktop/Новая папка/1/Program.cs
@@ -1,17 +1,17 @@
-﻿Console.WriteLine("Введите кол-во строк: ");
+Console.WriteLine("Введите кол-во строк: ");
 int num1 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите кол-во столбцов: ");
 int num2 = Convert.ToInt32(Console.ReadLine());
-FillArray(num1, num2);
+SortingArray(FillArray(num1, num2));
 
-double[,] FillArray(int line, int col)
+int[,] FillArray(int first, int second)
 {
-    double[,] array = new double[line, col];
+    int[,] array = new int[first, second];
     for (int i = 0; i < array.GetLength(0); i++)
     {
         for (int j = 0; j < array.GetLength(1); j++)
         {
-            array[i, j] = Math.Round(new Random().Next(-10, 11) + new Random().NextDouble(), 1);
+            array[i, j] = new Random().Next(1, 11);
             Console.Write(array[i, j] + "|");
         }
         Console.WriteLine();
@@ -19,3 +19,32 @@ double[,] FillArray(int line, int col)
     return array;
 }
 
+void SortingArray(int[,] arr)
+{
+    int temp = 0;
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1)-1; j++)
+        {
+            for (int k = j; k < arr.GetLength(1); k++)
+            {
+                if (arr[i, j] < arr[i, k])
+                {
+                    temp = arr[i, j];
+                    arr[i, j] = arr[i, k];
+                    arr[i, k] = temp;
+                }
+            }
+        }
+    }
+Console.WriteLine("Ответ: ");
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write(arr[i, j] + "|");
+        }
+        Console.WriteLine();
+    }
+
+}
